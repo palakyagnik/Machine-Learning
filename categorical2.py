d353263dce9769500efb25de071b4bfb9a9b37ea
palakyagnik <palak.yagnik143941@marwadiuniversity.ac.in>
2026-06-23 12:52:53 +0530

    Add files via upload

--- categorical2.py
+++ categorical2.py
@@ -1,23 +1,26 @@
-# Onehotencoder for mapping
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
-df=pd.read_csv('D:/MCA3/tempbook.csv')
+df = pd.read_csv("tempbook.csv")
+
 print(df)
 
-x=df[['color','size','price']].values
-color_le=LabelEncoder()
-x[:,0]=color_le.fit_transform(x[:,0])
+x = df[['color', 'size', 'price']].values
+
+color_le = LabelEncoder()
+x[:,0] = color_le.fit_transform(x[:,0])
+
 print(x)
 
+ct = ColumnTransformer(
+    [("color", OneHotEncoder(), [0])],
+    remainder='passthrough'
+)
 
-#One way with onehotencoder for creating dummy features with sklearn
-ct = ColumnTransformer([("color", OneHotEncoder(), [0])], remainder = 'passthrough')
 x = ct.fit_transform(x)
+
 print(x)
 
-#Other way with pandas
 print(pd.get_dummies(df[['color','price']]))
+ 
